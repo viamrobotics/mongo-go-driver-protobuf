b3cf8ff737232e920f780ed365fce2aace322eb8
Naveed Jooma <naveed@viam.com>
2021-11-10 17:19:23 -0500

    Updating timestamp codec

--- codecs.go
+++ codecs.go
@@ -4,12 +4,11 @@ import (
 	"reflect"
 	"time"
 
-	"github.com/golang/protobuf/ptypes"
-	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"go.mongodb.org/mongo-driver/bson/bsoncodec"
 	"go.mongodb.org/mongo-driver/bson/bsonrw"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	timestamp "google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/amsokol/mongo-go-driver-protobuf/pmongo"
 )
@@ -73,15 +72,11 @@ type timestampCodec struct {
 // EncodeValue encodes Protobuf Timestamp value to BSON value
 func (e *timestampCodec) EncodeValue(ectx bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
 	v := val.Interface().(timestamp.Timestamp)
-	t, err := ptypes.Timestamp(&v)
-	if err != nil {
-		return err
-	}
 	enc, err := ectx.LookupEncoder(timeType)
 	if err != nil {
 		return err
 	}
-	return enc.EncodeValue(ectx, vw, reflect.ValueOf(t.In(time.UTC)))
+	return enc.EncodeValue(ectx, vw, reflect.ValueOf(v.AsTime()))
 }
 
 // DecodeValue decodes BSON value to Timestamp value
@@ -94,10 +89,7 @@ func (e *timestampCodec) DecodeValue(ectx bsoncodec.DecodeContext, vr bsonrw.Val
 	if err = enc.DecodeValue(ectx, vr, reflect.ValueOf(&t).Elem()); err != nil {
 		return err
 	}
-	ts, err := ptypes.TimestampProto(t.In(time.UTC))
-	if err != nil {
-		return err
-	}
+	ts := timestamp.New(t)
 	val.Set(reflect.ValueOf(*ts))
 	return nil
 }
